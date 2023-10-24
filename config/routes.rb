0829efbcc5f31da8938c33f829bca1b1ef2792d2
Alejandro Piñango <alejandropinango@gmail.com>
2023-10-24 18:59:51 -0300

    Se creó el controlador y la vista admin view y los formularios para que el admin pueda crear, editar y listar users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'admin/create_user'
+  get 'admin/edit_user'
+  get 'admin/show_user'
   root 'pages#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   # root "articles#index"
+  # Permisos del admin
+  resources :admin, only: [:index, :new, :create, :update]
+  # Ruta para crear usuarios
+  post 'admin/create', to: 'admin#create', as: 'admin_create'
+
 end
---
+++ test/controllers/admin_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class AdminControllerTest < ActionDispatch::IntegrationTest
+  test "should get create_user" do
+    get admin_create_user_url
+    assert_response :success
+  end
+
+  test "should get edit_user" do
+    get admin_edit_user_url
+    assert_response :success
+  end
+
+  test "should get show_user" do
+    get admin_show_user_url
+    assert_response :success
+  end
+end
